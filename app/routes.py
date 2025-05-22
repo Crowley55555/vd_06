7d9f38a1198595ef3d7967b85cbd3fa260536868
Crowley55555 <lavrovartem0511@gmail.com>
2025-05-22 21:33:25 +0300

    home work is done

---
+++ app/routes.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from app import app
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Получаем данные из формы
+        name = request.form.get('name')
+        city = request.form.get('city')
+        hobby = request.form.get('hobby')
+        age = request.form.get('age')
+
+        # Передаем данные в шаблон
+        return render_template('index.html',
+                               name=name,
+                               city=city,
+                               hobby=hobby,
+                               age=age,
+                               submitted=True)
+
+    # Если GET-запрос, просто отображаем форму
+    return render_template('index.html', submitted=False)
+
+
